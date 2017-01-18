3cfa6428a507f0d6ddabe3f768062d96d42e61c8
JockFax32 <foxtjack@gmail.com>
2017-01-18 15:12:42 -0800

    Pushing for participation

--- health-burden/eda.R
+++ health-burden/eda.R
@@ -2,13 +2,14 @@
 # Feel free to follow these steps, or complete your own EDA
 
 # Set up (install packages if you don't have them)
+setwd("~/Desktop/School/University_Washington/2016-2017/Winter/INFO 370/Modules/EDA/eda/health-burden/data/")
 library(dplyr)
 library(tidyr)
 library(ggplot2)
 library(plotly)
 library(vioplot)
-setwd('~/Documents/info-370/eda/health-burden/')
-risk.data <- read.csv('./data/prepped/risk-data.csv', stringsAsFactors = FALSE) 
+risk.data <- read.csv('prepped/risk-data.csv', stringsAsFactors = FALSE) 
+risk.data <- as.data.frame(risk.data)
 
 ######################
 ### Data Structure ###
@@ -17,27 +18,43 @@ risk.data <- read.csv('./data/prepped/risk-data.csv', stringsAsFactors = FALSE)
 ## Using a variety of functions, investigate the structure of your data, such as:
 
 # Dimensions, column names, structure, summaries, etc.
+num.col <- ncol(risk.data)
+num.row <- nrow(risk.data)
+col.names <- colnames(risk.data)
+
+summary(risk.data, na.rm = TRUE)
 
 # Replacing missing values...?
+# new.risk <- na.omit(risk.data)
+risk.data[is.na(risk.data)] <- 0
 
 ###########################
 ### Univariate Analysis ###
 ###########################
 
 ## Using a variety of approaches, investigate the structure each (risk column) individually
+str(risk.data)
 
 # Summarize data
- 
+lapply(risk.data, summary)
+
 # Create histograms, violin plots, boxplots
-
-
+hist(risk.data$alcohol.use)
+hist(risk.data$low.exercise)
+hist(risk.data$high.meat)
 ####################################
 ### Univariate Analysis (by age) ###
 ####################################
 
 # Investiage how each risk-variable varies by **age group**
+risk.data <- risk.data %>% 
+  group_by(age) %>% 
+  summarise(risk.data$high.meat)
 
 # Create histograms, violin plots, boxplots. Calculate values as needed. 
+boxplot(high.meat ~ age, data = risk.data, las=2)
+boxplot(drug.use ~ age, data = risk.data, las=2)
+boxplot(low.exercise ~ age, data = risk.data, las=2)
 
 
 ####################################
@@ -45,6 +62,7 @@ risk.data <- read.csv('./data/prepped/risk-data.csv', stringsAsFactors = FALSE)
 ####################################
 
 # Investiage how each risk-variable varies by **sex**
+
 
 # Compare male to female values -- requires reshaping (and dropping population)!
 
@@ -54,10 +72,16 @@ risk.data <- read.csv('./data/prepped/risk-data.csv', stringsAsFactors = FALSE)
 ########################################
 
 ## Investiage how each risk-variable varies by **country**
-
 # Aggregate by country
+by.country <- risk.data %>%
+  group_by(country, country.code) %>% 		
+  summarize(smoking = sum(smoking * pop), pop=sum(pop)) %>% 		
+  ungroup() %>% 		
+  mutate(smoking.rate = smoking / pop)		
+
 
 # Create a choropleth map (see https://plot.ly/r/choropleth-maps/)
+
 
 ###########################
 ### Bivariate Analysis ####
